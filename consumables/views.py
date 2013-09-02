cfcf77af4054b3f97718514e433d256cf4d9a4ff
David Lewis <meng.lewis@gmail.com>
2013-09-02 02:03:19 +0000

    changed back to basic list view

--- consumables/views.py
+++ consumables/views.py
@@ -1,5 +1,5 @@
 from .models import Item, Stat
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 
 from rest_framework import viewsets
 from .serializers import ItemSerializer
@@ -15,16 +15,6 @@ class ItemListView(ListView):
 class ItemDetailView(DetailView):
     model = Item
     template_name = "item_detail.html"
-
-class FoodListView(ItemListView):
-    def get_queryset(self):
-        queryset = super(FoodListView, self).get_queryset()
-        return queryset.filter(consumables_type='FOOD')
-
-class UtilityListView(ItemListView):
-    def get_queryset(self):
-        queryset = super(UtilityListView, self).get_queryset()
-        return queryset.filter(consumables_type='UTIL')
 
 class ItemList(ListModelMixin, GenericAPIView):
     queryset = Item.objects.all()
